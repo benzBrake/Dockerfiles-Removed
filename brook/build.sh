bae81429f60af48cda091f9f408e1c624511ac10
Ryan Lieu <github-benzbrake@woai.ru>
2019-11-15 10:32:01 +0800

    添加latest标签

--- brook/build.sh
+++ brook/build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 cd $(dirname $(readlink -f $0))
 BROOK_VERSION=$(curl -s https://api.github.com/repos/txthinking/brook/releases/latest | grep "tag_name" | cut -d \" -f 4)
-docker build -t ${DOCKER_USERNAME-benzbrake}/brook:${BROOK_VERSION} -t latest .
+docker build -t ${DOCKER_USERNAME-benzbrake}/brook:${BROOK_VERSION} .
+docker tag ${DOCKER_USERNAME-benzbrake}/brook:${BROOK_VERSION} ${DOCKER_USERNAME-benzbrake}/brook:latest
+docker images
 docker login -u ${DOCKER_USERNAME-benzbrake} -p ${DOCKER_PASSWORD}
 docker push ${DOCKER_USERNAME-benzbrake}/brook:${BROOK_VERSION}
 docker push ${DOCKER_USERNAME-benzbrake}/brook:latest
